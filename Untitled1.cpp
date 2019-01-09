aad3724569e8b5c51c13fa85628141849cfea650
TranNgocToan <46474196+TranNgocToan@users.noreply.github.com>
2019-01-09 13:53:51 +0700

    sửa
    bài sửa của em thầy ơi

---
+++ Untitled1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace s	td;
+int main()
+{ float a,b,c,d;
+	cout<<"nhap tien gui:";
+	cin>>a;
+	cout<<"nhap ky vong:";
+	cin>>b;
+	cout<<"nhap lai suat:";
+	cin>>c;
+	d=(b-a)/(a*c);
+	cout<<"so nam:"<<d;
+return 0;
+	
+} 
